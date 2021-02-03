606f50e8d7408508478cda2959728b757c4aad09
Ktunezawa <rapurasnohako@gmail.com>
2021-02-03 08:14:08 +0000

    Finish Search

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'searchs/search'
+  get 'search/search'
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
   resources :users
   resources :books
+  get '/search', to: 'search#search'
 end
